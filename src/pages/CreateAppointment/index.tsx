fd1d43f3eb4bda5e47d81826d7d7483d0284d60a
Danilo Pereira <danilopx1@gmail.com>
2020-07-27 22:47:03 -0300

    Criando picker de data

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,6 +1,10 @@
 import React, { useCallback, useEffect, useState } from 'react';
+
+import DateTimePicker from '@react-native-community/datetimepicker';
+
 import { useRoute, useNavigation } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/Feather'
+import Icon from 'react-native-vector-icons/Feather';
+import { Platform } from 'react-native';
 import { useAuth } from '../../hooks/auth';
 import api from '../../services/api';
 import {
@@ -14,6 +18,10 @@ import {
   ProviderContainer,
   ProviderAvatar,
   ProviderName,
+  Calendar,
+  Title,
+  OpenDatePickerButton,
+  OpenDatePickerButtonText,
 } from './styles';
 
 interface RouteParams {
@@ -27,14 +35,18 @@ export interface Provider {
 }
 
 const CreateAppointment: React.FC = () => {
-  const { user }= useAuth();
+  const { user } = useAuth();
   const route = useRoute();
   const { goBack } = useNavigation();
 
   const routeParams = route.params as RouteParams;
 
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const [providers, setProviders] = useState<Provider[]>([]);
-  const [selectedProvider, setSelectedProvider] = useState(routeParams.providerId);
+  const [selectedProvider, setSelectedProvider] = useState(
+    routeParams.providerId,
+  );
 
   useEffect(() => {
     api.get('/providers').then(response => {
@@ -42,47 +54,81 @@ const CreateAppointment: React.FC = () => {
     });
   }, []);
 
-
   const navigateback = useCallback(() => {
     goBack();
-  },[goBack]);
+  }, [goBack]);
 
-  const handleSelectProvider = useCallback((providerId: string)=>{
-    setSelectedProvider(providerId)
-  },[])
+  const handleSelectProvider = useCallback((providerId: string) => {
+    setSelectedProvider(providerId);
+  }, []);
+
+  const handleToggleDatePicker = useCallback(() => {
+    setShowDatePicker(state => !state);
+  }, []);
+
+  const handleDateChanged = useCallback(
+    (event: any, date: Date | undefined) => {
+      if (Platform.OS === 'android') {
+        setShowDatePicker(false);
+      }
+      if (date) {
+        setSelectedDate(date);
+      }
+      // da te
+    },
+    [],
+  );
 
   return (
     <Container>
       <Header>
         <BackButton onPress={navigateback}>
           <Icon name="chevron-left" size={24} color="#999591" />
-          </BackButton>
-          <HeaderTitle>Cabeleireiros</HeaderTitle>
+        </BackButton>
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
 
-          <UserAvatar source={{uri: user.avatar_url}} />
-
-
+        <UserAvatar source={{ uri: user.avatar_url }} />
       </Header>
 
       <ProvidersListContainer>
-      <ProvidersList
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        data={providers}
-        keyExtractor={(provider) => provider.id}
-        renderItem={({item: provider})=>(
-          <ProviderContainer
-            onPress={() => handleSelectProvider(provider.id)}
-            selected={provider.id === selectedProvider}
-          >
-            <ProviderAvatar source={{uri: provider.avatar_url}} />
-            <ProviderName selected={provider.id === selectedProvider}>{provider.name}</ProviderName>
-          </ProviderContainer>
-        )}
-      />
+        <ProvidersList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={providers}
+          keyExtractor={provider => provider.id}
+          renderItem={({ item: provider }) => (
+            <ProviderContainer
+              onPress={() => handleSelectProvider(provider.id)}
+              selected={provider.id === selectedProvider}
+            >
+              <ProviderAvatar source={{ uri: provider.avatar_url }} />
+              <ProviderName selected={provider.id === selectedProvider}>
+                {provider.name}
+              </ProviderName>
+            </ProviderContainer>
+          )}
+        />
       </ProvidersListContainer>
+
+      <Calendar>
+        <Title>Escolha a data</Title>
+        <OpenDatePickerButton onPress={handleToggleDatePicker}>
+          <OpenDatePickerButtonText>
+            Selecionar outra data
+          </OpenDatePickerButtonText>
+        </OpenDatePickerButton>
+        {showDatePicker && (
+          <DateTimePicker
+            mode="date"
+            display="calendar"
+            onChange={handleDateChanged}
+            textColor="#f4ede8"
+            value={selectedDate}
+          />
+        )}
+      </Calendar>
     </Container>
-    )
+  );
 };
 
 export default CreateAppointment;
--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { FlatList } from 'react-native';
-import { Provider } from './index';
 import { RectButton } from 'react-native-gesture-handler';
+import { Provider } from './index';
 
 interface ProviderContainerProps {
   selected: boolean;
@@ -26,9 +26,7 @@ export const Header = styled.View`
   align-items: center;
 `;
 
-export const BackButton = styled.TouchableOpacity`
-
-`;
+export const BackButton = styled.TouchableOpacity``;
 
 export const HeaderTitle = styled.Text`
   color: #f5ede8;
@@ -38,7 +36,6 @@ export const HeaderTitle = styled.Text`
 `;
 
 export const UserAvatar = styled.Image`
-
   width: 56px;
   height: 56px;
   border-radius: 28px;
@@ -54,7 +51,7 @@ export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
 `;
 
 export const ProviderContainer = styled(RectButton)<ProviderContainerProps>`
-  background: ${props => props.selected? '#ff9000' : '#3e3b47'};
+  background: ${props => (props.selected ? '#ff9000' : '#3e3b47')};
   flex-direction: row;
   align-items: center;
   padding: 8px 12px;
@@ -72,8 +69,29 @@ export const ProviderName = styled.Text<ProviderNameProps>`
   margin-left: 8px;
   font-family: 'RobotoSlab-Medium';
   font-size: 16px;
-  color: ${props => props.selected? '#232129' : '#f4ede8'};
-
-
+  color: ${props => (props.selected ? '#232129' : '#f4ede8')};
 `;
 
+export const Calendar = styled.View``;
+
+export const Title = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  color: #f4ede8;
+  font-size: 24px;
+  margin: 0 24px 24px;
+`;
+
+export const OpenDatePickerButton = styled(RectButton)`
+  height: 46px;
+  background: #ff9000;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+  margin: 0 24px;
+`;
+
+export const OpenDatePickerButtonText = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 16px;
+  color: #232129;
+`;
